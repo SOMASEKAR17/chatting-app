d045fe1fc32c9a7bbf0b80fcde60fafea7035428
SOMASEKAR17 <somasekarnaidu79@gmail.com>
2025-05-13 15:11:08 +0530

    done with auth controllers

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcrypt from 'bcryptjs';
 import {generateToken} from '../lib/utils.js'
+import cloudinary from '../lib/cloudinary.js';
 
 export const signup = async (req,res)=>{
     const {fullName,email,password} = req.body;
@@ -9,11 +10,15 @@ export const signup = async (req,res)=>{
             return res.status(500).json({message:"All fields are required"});
         }
         // create the user -> hashing the password -> create the token -> send the response
-        const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
         const user = await User.findOne({email});
         if (user){
             return res.status(400).json({message:"User already exists"});
         }
+        const gmailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+        if(!gmailRegex.test(email)){
+            return res.status(400).json({message:"Please enter a valid gmail address"});
+        }
+        const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
         if (!regex.test(password)){
             return res.status(400).json({message:"Password is weak"});
         }
@@ -46,10 +51,61 @@ export const signup = async (req,res)=>{
     }
 }
 
-export const login = (req,res)=>{
-    res.send('login route');
+export const login = async(req,res)=>{
+    const {email,password} = req.body;
+    try {
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({message:"Invalid credentials"});
+        }
+        const isPasswordCorrect = await bcrypt.compare(password,user.password);
+        if(!isPasswordCorrect){
+            return res.status(400).json({message:"Invalid credentials"});
+        }
+        generateToken(user._id,res)
+        res.status(200).json({
+            _id:user._id,
+            fullName:user.fullName,
+            email:user.email,
+            profilePic:user.profilePic
+        })
+    } catch (error) {
+        console.log("Error in login controller",error.message);
+        res.status(500).json({message:"Internal server error"});
+    }
 }
 
 export const logout = (req,res)=>{
-    res.send('logout route');
+    try {
+        res.cookie("token","",{maxAge:0});
+        res.status(200).json({message:"Logout successfull"});
+    } catch (error) {
+        res.status(500).json({message:"Internal Server Error"});
+    }
+}
+
+export const updateProfile = async (req,res)=>{
+    try {
+        const {profilePic}=req.body;
+        const userId = req.user._id;
+
+        if(!profilePic){
+            return res.status(400).json({message:"Please provide a profile picture"});
+        }
+        const uploadResoponse = await cloudinary.uploader.upload(profilePic);
+        const updatedUser = await User.findByIdAndUpdate(userId,{profilePic:uploadResoponse.secure_url},{new:true});
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        console.log("Error in updateProfile controller",error.message);
+        res.status(500).json({message:"Internal server error"});
+    }
+}
+
+export const checkAuth = (req,res)=>{
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in checkAuth controller",error.message);
+        res.status(500).json({message:"Internal server error"});
+    }
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import {connectDB} from './lib/db.js'
-// to use this you will have to change the type in pakage.json to module , else you have to use const express = require('express');
-
-dotenv.config()
 import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
+// to use this you will have to change the type in pakage.json to module , else you have to use const express = require('express');
 const app = express();
+dotenv.config()
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
+app.use(cookieParser());
+
 
 app.use('/api/auth',authRoutes);
 
 const PORT=process.env.PORT;
 
 app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`);
+console.log(`server is running on port ${PORT}`);
     connectDB()
 })
